4c90a3072b529aad7131a85de64380e1de07f36c
Daniel Fitzgerald <daniel.fitzgerald@hotmail.ca>
2014-07-01 10:38:58 -0230

    - Added unused comments

--- True or false multiplayer/src/io/github/brother_daniel/multiTF/game/ChatCodes.java
+++ True or false multiplayer/src/io/github/brother_daniel/multiTF/game/ChatCodes.java
@@ -3,8 +3,8 @@ package io.github.brother_daniel.multiTF.game;
 public class ChatCodes {
 	public static String NEW_GAME = "NEW_GAME";
 	public static String JOIN_GAME = "JOIN_GAME";
-	public static String HOST_FIRST_GUESS = "HOST_FIRST_GUESS";
-	public static String CLIENT_FIRST_GUESS = "CLIENT_FIRST_GUESS";
+	public static String HOST_FIRST_GUESS = "HOST_FIRST_GUESS"; // This is unused
+	public static String CLIENT_FIRST_GUESS = "CLIENT_FIRST_GUESS"; // This is unused
 	public static String ANSWER_TRUE = "ANSWER_TRUE";
 	public static String ANSWER_FALSE = "ANSWER_FALSE";
 	public static String QUESTION_ANSWER = "§";
